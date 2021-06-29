5d9e9f65629e544eb144bae17a1c3e9ff72aafe3
Sanskar <sanskar.shubham28@gmail.com>
2021-06-29 21:19:36 +0530

    Description added

--- SomeGoodQuestions/CircularMatrix.cpp
+++ SomeGoodQuestions/CircularMatrix.cpp
@@ -1,4 +1,10 @@
 //Print matrix in circular manner
+// i.e. if matrix is    1  2  3  4 
+//                      5  6  7  8
+//                      9  10 11 12 
+//                      13 14 15 16
+//
+//You have to print 1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10
 
 #include <bits/stdc++.h>
 using namespace std;
